fcd5cb7fd70ee01885d9d65d20afa1707be78edb
Drew Bonde <abonde@123.net>
2026-07-24 20:26:37 -0400

    uses vite command variable in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     vue(),
     vuetify({
       autoImport: true,
     }),
   ],
-  base: process.env.NODE_ENV === 'production'
+
+  base: command === 'build'
     ? '/rebirth/'
     : '/',
-})
+}))
