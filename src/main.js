32b924b828997914785b14758772126ab0926f9c
geech <abonde@123.net>
2026-07-28 11:50:02 -0400

    route fix

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+const redirectPath = sessionStorage.getItem('redirectPath')
+
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath')
+  window.history.replaceState(null, '', redirectPath)
+}
+
 createApp(App).use(router).use(vuetify).mount('#app')
